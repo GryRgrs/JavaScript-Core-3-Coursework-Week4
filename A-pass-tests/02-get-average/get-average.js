3dbed76b452cc92f0779c90d3d434afd36733b2f
GryRgrs <102528828+GryRgrs@users.noreply.github.com>
2022-11-09 20:24:20 +0000

    first 3 challenges work in console but not in npm testing

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,16 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+let average = 0
+for (i=0; i < numbers.length; i++){
+let currentNumber = numbers[i]
+i += currentNumber;}
+average = average / numbers.length;
+{
+return average;  
+}
+}
+
 
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,14 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+let average = 0;
+for (i = 0; i < carsSold.length; i++) {
+let currentNumber = carsSold[i];
+i += currentNumber;
+}
+sales = sales / carsSold.length;
+{
+return sales;
+}
+}
+
 
 module.exports = sales;
